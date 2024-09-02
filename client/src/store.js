b5bcbeffa235117fc9452d159b05bd0516253a3c
Vigneshshankaran <vigneshganesha@gmail>
2024-09-02 11:36:06 +0530

    new

--- client/src/store.js
+++ client/src/store.js
@@ -1,63 +1,74 @@
-import { combineReducers } from 'redux';
-import { createStore, applyMiddleware } from 'redux';
-import {thunk} from 'redux-thunk';
+import { createStore, applyMiddleware, combineReducers } from 'redux';
+import thunk from 'redux-thunk';
 import { composeWithDevTools } from 'redux-devtools-extension';
-import { 
-  getAllPizzasReducer, 
-  addPizzaReducer, 
-  getPizzaByIdReducer, 
-  editPizzaReducer 
-} from './reducers/pizzaReducers';
-import { cartReducer } from './reducers/cartReducer';
-import { 
-  loginUserReducer, 
-  registerUserReducer, 
-  getAllUsersReducer 
-} from './reducers/userReducer';
-import { 
-  placeOrderReducer, 
-  getUserOrdersReducer, 
-  getAllOrdersReducer 
-} from './reducers/orderReducer';
 
-// Combine reducers
-const rootReducer = combineReducers({
+import {
   getAllPizzasReducer,
   addPizzaReducer,
   getPizzaByIdReducer,
   editPizzaReducer,
-  cartReducer,
+} from './reducers/pizzaReducers';
+
+import { cartReducer } from './reducers/cartReducer';
+import {
   loginUserReducer,
   registerUserReducer,
   getAllUsersReducer,
+} from './reducers/userReducer';
+
+import {
   placeOrderReducer,
   getUserOrdersReducer,
   getAllOrdersReducer,
+} from './reducers/orderReducer';
+
+// Utility to load initial state from localStorage
+const loadFromLocalStorage = (key, defaultValue) => {
+  const storedData = localStorage.getItem(key);
+  return storedData ? JSON.parse(storedData) : defaultValue;
+};
+
+// Combine reducers
+const rootReducer = combineReducers({
+  pizzas: combineReducers({
+    all: getAllPizzasReducer,
+    add: addPizzaReducer,
+    byId: getPizzaByIdReducer,
+    edit: editPizzaReducer,
+  }),
+  cart: cartReducer,
+  user: combineReducers({
+    login: loginUserReducer,
+    register: registerUserReducer,
+    allUsers: getAllUsersReducer,
+  }),
+  orders: combineReducers({
+    placeOrder: placeOrderReducer,
+    userOrders: getUserOrdersReducer,
+    allOrders: getAllOrdersReducer,
+  }),
 });
 
 // Initial state from localStorage
-const cartItems = localStorage.getItem('cartItems') 
-  ? JSON.parse(localStorage.getItem('cartItems')) 
-  : [];
-
-const currentUser = localStorage.getItem('currentUser') 
-  ? JSON.parse(localStorage.getItem('currentUser')) 
-  : null;
-
 const initialState = {
-  cartReducer: {
-    cartItems,
+  cart: {
+    cartItems: loadFromLocalStorage('cartItems', []),
   },
-  loginUserReducer: {
-    currentUser,
+  user: {
+    login: {
+      currentUser: loadFromLocalStorage('currentUser', null),
+    },
   },
 };
+
+// Middleware
+const middleware = [thunk];
 
 // Create store with middleware and Redux DevTools extension
 const store = createStore(
   rootReducer,
   initialState,
-  composeWithDevTools(applyMiddleware(thunk))
+  composeWithDevTools(applyMiddleware(...middleware))
 );
 
 export default store;
