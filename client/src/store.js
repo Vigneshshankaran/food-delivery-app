08bc0fd6c0eb04e5b1ee62225f96d4787be9728b
Vigneshshankaran <vigneshganesha@gmail>
2024-09-03 10:03:51 +0530

    new

--- client/src/store.js
+++ client/src/store.js
@@ -2,6 +2,7 @@ import { combineReducers } from 'redux';
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import { composeWithDevTools } from 'redux-devtools-extension';
+
 import { 
   getAllPizzasReducer, 
   addPizzaReducer, 
@@ -19,6 +20,7 @@ import {
   getUserOrdersReducer, 
   getAllOrdersReducer 
 } from './reducers/orderReducer';
+
 
 // Combine reducers
 const rootReducer = combineReducers({
